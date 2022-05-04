59a5cdb5e1edae20aafdd796cc5aaec62a7c6264
nicolasggdev <nicolasggdev@gmail.com>
2022-05-04 15:33:50 -0500

    fix: fixing performance issues

--- src/Components/SearchBox/SearchBox.jsx
+++ src/Components/SearchBox/SearchBox.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import swal from "sweetalert";
 
 // Styles
@@ -29,7 +29,9 @@ const SearchBox = ({ filterResident }) => {
     }
   };
 
-  filterResident(data, filter, filterType);
+  useEffect(() => {
+    filterResident(data, filter, filterType);
+  }, [filterResident, data, filter, filterType]);
 
   const search = () => {
     if (filterType === "name") {
